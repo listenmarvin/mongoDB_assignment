fc1c21f0de65886361bf615424e60c9cdf8a2dba
listenmarvin <listenmarvin.int@niveussolutions.com>
2023-05-04 11:14:07 +0530

    first commit MongoDb

--- app.js
+++ app.js
@@ -28,19 +28,3 @@ module.exports = app;
 
 
 
-// const app = require("./app");
-// const monoose = require("mongoose");
-
-// monoose
-//   .connect(
-//     "mongodb+srv://firstDb:database@cluster0.rqbq2ea.mongodb.net/Book?retryWrites=true&w=majority"
-//   )
-//   .then(() => {
-//     console.log("connected to mongoDB");
-//     app.listen(3000, () => {
-//       console.log("Server Started:Port is 3000");
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
